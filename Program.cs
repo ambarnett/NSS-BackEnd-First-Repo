2b3c5f3d9c8e7618e7458f7301ce7fc95ea25cc3
ambarnett <andy.barnett09@gmail.com>
2021-08-20 10:58:21 -0500

    inital commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Console.WriteLine("Who would you like to say hello to?");
+
+string name = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name)){
+    Console.WriteLine("Fine, don't say 'hello'!");
+} else {
+    Console.WriteLine($"Hello, {name}!");
+}
